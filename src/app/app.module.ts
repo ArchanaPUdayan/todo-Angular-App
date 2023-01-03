3a2637ecfd6658eeeca410dc66d155882f4092a0
Archanapudayan <atchanaudayan64534@gmail.com>
2023-01-03 20:18:29 +0530

    viewall

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoEntryComponent } from './todo-entry/todo-entry.component';
+import { ViewalltodoComponent } from './viewalltodo/viewalltodo.component';
+
+const myRoute:Routes=[
+  {
+    path:"",
+    component:TodoEntryComponent
+  },
+  {
+    path:"view",
+    component:ViewalltodoComponent
+
+  }
+]
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoEntryComponent
+    TodoEntryComponent,
+    ViewalltodoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
